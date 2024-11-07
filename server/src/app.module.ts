75163cc23a0892f099ea5bd2086dc77daf100951
Alessia <98785381+amorea9@users.noreply.github.com>
2024-11-07 10:54:28 +0100

    added db connection - wip

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const daosUri = 'mongodb://127.0.0.1:27017/DAOS';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(daosUri)],
   controllers: [AppController],
   providers: [AppService],
 })
