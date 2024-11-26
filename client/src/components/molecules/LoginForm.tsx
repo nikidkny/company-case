a71ad3ce274a3dffb54e92e7e691d9b222ffa877
Alessia <98785381+amorea9@users.noreply.github.com>
2024-11-26 14:20:23 +0100

    added id and name for textInput

--- client/src/components/molecules/LoginForm.tsx
+++ client/src/components/molecules/LoginForm.tsx
@@ -8,45 +8,23 @@ interface LoginFormProps {
   onSubmit: (e: React.FormEvent) => Promise<void>;
 }
 
-export default function LoginForm({
-  formData,
-  onChange,
-  onSubmit,
-}: LoginFormProps) {
+export default function LoginForm({ formData, onChange, onSubmit }: LoginFormProps) {
   return (
     <div className="flex flex-col items-center">
-      <form
-        onSubmit={onSubmit}
-        className="flex flex-col w-[93vw] p-4 space-y-4 bg-white rounded-md"
-      >
+      <form onSubmit={onSubmit} className="flex flex-col w-[93vw] p-4 space-y-4 bg-white rounded-md">
         <TextHeadline variant="h1" size="lg">
           Login
         </TextHeadline>
 
         <div className="w-80">
-          <TextInput
-            inputType="email"
-            value={formData.email}
-            onChange={(value) => onChange("email", value)}
-            placeholder="Enter your email"
-          />
+          <TextInput inputType="email" value={formData.email} onChange={(value) => onChange("email", value)} placeholder="Enter your email" id="email" name="email" />
         </div>
 
         <div className="w-80">
-          <TextInput
-            inputType="password"
-            value={formData.password}
-            onChange={(value) => onChange("password", value)}
-            placeholder="Enter your password"
-          />
+          <TextInput inputType="password" value={formData.password} onChange={(value) => onChange("password", value)} placeholder="Enter your password" id="password" name="password" />
         </div>
 
-        <Button
-          buttonState="default"
-          buttonVariant="primary"
-          buttonLabel="Login"
-          iconPosition="none"
-        />
+        <Button buttonState="default" buttonVariant="primary" buttonLabel="Login" iconPosition="none" />
       </form>
     </div>
   );
