1023c063947e2a772b21cd9f3134bf3b748c29ae
andreaDiClaudio <andreadicla@gmail.com>
2024-12-05 16:51:05 +0100

    left commment

--- client/src/utilities/errorUtils.ts
+++ client/src/utilities/errorUtils.ts
@@ -1,7 +1,7 @@
-export const getFieldErrorMessage = (errorMessages: string[] | null, fieldName: string): string | null => {
-    if (!errorMessages) return null;
-    const lowerCaseField = fieldName.toLowerCase();
-    // Match the field name in the error messages
-    return errorMessages.find((msg) => msg.toLowerCase().includes(lowerCaseField)) || null;
-  };
-  
+// The getFieldErrorMessage function matches specific field error messages by a substring of the message that corresponds to the field name, allowing correct validation for each field.
+export const getFieldErrorMessage = (errorMessages: string[] | null, errorSubstring: string): string | null => {
+  if (!errorMessages) return null;
+  const lowerCaseField = errorSubstring.toLowerCase();
+  // Match the field name in the error messages
+  return errorMessages.find((msg) => msg.toLowerCase().includes(lowerCaseField)) || null;
+};
