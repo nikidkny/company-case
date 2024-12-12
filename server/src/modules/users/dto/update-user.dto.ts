a097b8ac55101a3c8754c1a8754642a3431706c3
Nikolett Dékány <d.nikolett2@gmail.com>
2024-12-12 13:38:43 +0100

    update user update validaion based on create

--- server/src/modules/users/dto/update-user.dto.ts
+++ server/src/modules/users/dto/update-user.dto.ts
@@ -1,36 +1,43 @@
 import { Type } from 'class-transformer';
 import {
   IsOptional,
+  IsString,
   IsNotEmpty,
   IsEmail,
   IsDate,
   IsBoolean,
+  MinLength,
 } from 'class-validator';
 
 export class UpdateUserDto {
   @IsOptional()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'First name should not be empty' })
+  @IsString({ message: 'First name must be a string' })
   firstName?: string;
-
   @IsOptional()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Last name should not be empty' })
+  @IsString({ message: 'Last name must be a string' })
   lastName?: string;
-
   @IsOptional()
-  @IsEmail()
+  @IsNotEmpty({ message: 'Email should not be empty' })
+  @IsEmail({}, { message: 'Email must be a valid email address' })
   email?: string;
-
   @IsOptional()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Password should not be empty' })
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
   password?: string;
-
   @IsOptional()
-  @IsDate()
+  @IsNotEmpty({ message: 'Confirm password should not be empty' })
+  confirmPassword?: string;
+  @IsOptional()
+  @IsNotEmpty({ message: 'Birthdate should not be empty' })
   @Type(() => Date)
+  @IsDate({ message: 'Birthdate must be a valid date' })
   birthdate?: Date;
 
   @IsOptional()
-  @IsBoolean()
+  @IsNotEmpty({ message: 'Availability should not be empty' })
+  @IsBoolean({ message: 'isAvailable must be a boolean value' })
   isAvailable?: boolean;
 
   @IsOptional()
