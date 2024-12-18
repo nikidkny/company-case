227d1db1facbaf556bd110dc0df2da9e91cb5052
andreaDiClaudio <andreadicla@gmail.com>
2024-12-18 09:58:19 +0100

    moved getErrorMessage in utility file to make it reusable

--- client/src/utilities/auth.ts
+++ client/src/utilities/auth.ts
@@ -4,3 +4,18 @@ import CryptoJS from "crypto-js"
   export const hashPassword = (password: string): string => {
     return CryptoJS.SHA256(password).toString();
   };
+
+  // Function to get an array of messages and an array of error to map and return the error message that matches the errorMap
+  export const getCustomErrorMessage = (
+    errorMessages: string[] | null,
+    errorMap: Record<string, string>
+  ): string | null => {
+    if (!errorMessages) return null;
+  
+    for (const error of errorMessages) {
+      if (errorMap[error]) {
+        return errorMap[error];
+      }
+    }
+    return null;
+  };
