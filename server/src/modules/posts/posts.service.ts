94355efeca27b00a9b1ec89da4ebcd3fdf029efe
Nikolett Dékány <d.nikolett2@gmail.com>
2024-12-19 11:19:06 +0100

    fixes for the comments

--- server/src/modules/posts/posts.service.ts
+++ server/src/modules/posts/posts.service.ts
@@ -33,7 +33,7 @@ export class PostsService {
       );
       return {
         post,
-        ensemble: ensemblePost?.ensembleId || null,
+        ensemble: ensemblePost?.ensembleId,
       };
     });
   }
